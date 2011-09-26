761b67b210ac40c8997897da2c4f151f18646676
Eugene Leonovich <gen.work@gmail.com>
2011-09-26 22:04:48 +0200

    added more tests to TaskTest class

--- tests/TaskQueue/Tests/Task/TaskTest.php
+++ tests/TaskQueue/Tests/Task/TaskTest.php
@@ -6,21 +6,50 @@ use TaskQueue\Task\Task;
 
 class TaskTest extends \PHPUnit_Framework_TestCase
 {
-    public function testReschedule()
+    public function testConstructor()
+    {
+        try {
+            $task = new Task();
+            $this->fail('__construct() throws a warning and notice if payload is not specified');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf('\PHPUnit_Framework_Error_Warning', $e, '__construct() throws a warning and notice if the payload is not specified');
+        }
+
+        $payload = 'payload';
+        $task = new Task($payload);
+        $this->assertSame($payload, $task->getPayload());
+
+        $eta = new \DateTime();
+        $task = new Task(null, $eta);
+        $this->assertSame($eta, $task->getEta());
+    }
+
+    public function testSetEta()
     {
         $eta = new \DateTime();
-        $retryDelaySec = 2;
+        $task = new Task(null, $eta);
+        $this->assertSame($eta, $task->getEta());
+    }
+
+    public function testRescheduling()
+    {
+        $eta = new \DateTime();
 
         $task = new Task(null, $eta);
-        $task->setMaxRetryCount(1);
-        $task->setRetryDelay($retryDelaySec);
+        $task->setMaxRetryCount(2);
 
+        $task->setRetryDelay(0);
         $this->assertTrue($task->reschedule());
         $this->assertEquals(1, $task->getRetryCount());
-        $this->assertEquals($eta->getTimestamp() + $retryDelaySec, $task->getEta()->getTimestamp());
+        $this->assertEquals($eta->getTimestamp(), $task->getEta()->getTimestamp());
+
+        $task->setRetryDelay(5);
+        $this->assertTrue($task->reschedule());
+        $this->assertEquals(2, $task->getRetryCount());
+        $this->assertEquals($eta->getTimestamp() + 5, $task->getEta()->getTimestamp());
 
         $this->assertFalse($task->reschedule());
-        $this->assertEquals(1, $task->getRetryCount());
-        $this->assertEquals($eta->getTimestamp() + $retryDelaySec, $task->getEta()->getTimestamp());
+        $this->assertEquals(2, $task->getRetryCount());
+        $this->assertEquals($eta->getTimestamp() + 5, $task->getEta()->getTimestamp());
     }
 }
